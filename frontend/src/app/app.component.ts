eef9b13e5e5ec40136fe5bbc8f20a3c17dc4567f
David Markel <david.l.markel@gmail.com>
2023-01-22 15:33:33 -0500

    Angular is EZ

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+  public counter = 0;
+
+  public onClick() {
+    this.counter += 1;
+    console.log("Counter: ", this.counter)
+  }
 }
