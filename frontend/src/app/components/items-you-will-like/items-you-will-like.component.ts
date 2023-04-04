e1afaa2d7afca3b7422cb472a74f62bb5686606c
David Markel <david.l.markel@gmail.com>
2023-04-03 20:40:08 -0400

    functinality for for you

--- frontend/src/app/components/items-you-will-like/items-you-will-like.component.ts
+++ frontend/src/app/components/items-you-will-like/items-you-will-like.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import {
+  animate,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+
 
 interface ItemData {
   imageSrc: string;
@@ -7,12 +15,51 @@ interface ItemData {
   price: number;
 }
 
+const fadeIn = trigger('fadeIn', [
+  transition(':enter', [
+    style({ opacity: 0 }),
+    animate('500ms', style({ opacity: 1 })),
+  ]),
+]);
+
 @Component({
   selector: 'app-items-you-will-like',
   templateUrl: './items-you-will-like.component.html',
-  styleUrls: ['./items-you-will-like.component.scss']
+  styleUrls: ['./items-you-will-like.component.scss'],
+  animations: [fadeIn],
 })
 export class ItemsYouWillLikeComponent {
+  @Output() itemClicked = new EventEmitter<any>();
+
+  onItemClick(item: any) {
+    this.itemClicked.emit(item);
+  }
+
+  constructor() {}
+
+
+  currentPage = 0;
+
+  onLeftArrowClick() {
+    const maxPages = Math.ceil(this.itemData.length / 3) - 1;
+    if (this.currentPage === 0) {
+      this.currentPage = maxPages;
+    } else {
+      this.currentPage--;
+    }
+  }
+  
+  onRightArrowClick() {
+    const maxPages = Math.ceil(this.itemData.length / 3) - 1;
+    if (this.currentPage === maxPages) {
+      this.currentPage = 0;
+    } else {
+      this.currentPage++;
+    }
+  }
+  
+  
+
   itemData: ItemData[] = [
     {
       imageSrc: '../../assets/itemImages/blackCouch.jpg',
@@ -31,6 +78,42 @@ export class ItemsYouWillLikeComponent {
       title: 'Yellow Dress',
       description: 'A sunny yellow dress',
       price: 39.99
-    }
+    },
+    {
+      imageSrc: "../../assets/items/itemImages/games/The_Legend_of_Zelda_Breath_of_the_Wild.jpg",
+      title: "The Legend of Zelda: Breath of the Wild",
+      description: "An action-adventure game where players control Link and explore the vast open world of Hyrule",
+      price: 59.99
+  },
+  {
+    imageSrc: "../../assets/items/itemImages/furniture/Accent_Chair.jpg",
+    title: "Accent Chair",
+    description: "A unique and stylish accent chair, perfect for adding personality to any room",
+    price: 399.99
+    },
+    {
+      imageSrc: "../../assets/items/itemImages/furniture/Bar_Cabinet.jpg",
+      title: "Bar Cabinet",
+      description: "A sophisticated and functional bar cabinet, great for storing and displaying liquor and barware",
+      price: 899.99
+      },
+      {
+        imageSrc: "../../assets/items/itemImages/household/HEPA_Air_Purifier.jpg",
+        title: "HEPA Air Purifier",
+        description: "A compact HEPA air purifier to keep your home fresh and allergen-free",
+        price: 99.99
+    },
+    {
+      imageSrc: "../../assets/items/itemImages/toys/Nerf_Gun.jpg",
+      title: "Nerf Gun",
+      description: "A powerful Nerf gun with foam darts for exciting battles",
+      price: 29.99
+  },
+  {
+    imageSrc: "../../assets/items/itemImages/groceries/Organic_Milk.jpg",
+    title: "Organic Milk",
+    description: "A half-gallon of organic milk for a healthier and fresher option",
+    price: 3.99
+    },
   ];
 }
