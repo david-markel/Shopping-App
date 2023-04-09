3f32b234ff1807ae684614e9b23207120eb178ec
David Markel <david.l.markel@gmail.com>
2023-04-09 19:03:35 -0400

    clean-up

--- frontend/src/app/components/item-detail/item-detail.component.ts
+++ frontend/src/app/components/item-detail/item-detail.component.ts
@@ -1,18 +1,36 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
   styleUrls: ['./item-detail.component.scss']
 })
-export class ItemDetailComponent {
+export class ItemDetailComponent implements OnInit {
   @Input() selectedItem: any;
   @Output() closeButtonClick = new EventEmitter<void>();
+  
+  rating: number = 0;
+  filledStars: number[] = [];
+  emptyStars: number[] = [];
 
   onCloseButtonClick() {
     this.closeButtonClick.emit();
   }
 
+  ngOnInit() {
+    this.rating = this.randomRating();
+    this.updateStars();
+  }
+
+  randomRating(): number {
+    return 1 + Math.random() * 4;
+  }
+
+  updateStars() {
+    const roundedRating = Math.round(this.rating);
+    this.filledStars = Array(roundedRating).fill(0);
+    this.emptyStars = Array(5 - roundedRating).fill(0);
+  }
+
   constructor() {}
-  
 }
