5ab90504874dc1755831ab5377f75e874593303e
David Markel <david.l.markel@gmail.com>
2023-05-25 15:36:56 -0400

    Orders

--- frontend/src/app/components/orders/orders.component.ts
+++ frontend/src/app/components/orders/orders.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+// orders.component.ts
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  orders: any[] = [];
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders(): void {
+    this.orders = [
+      { id: '1', status: 'Delivered', customer: 'John Doe', date: '2023-05-10', total: 100.00 },
+      { id: '2', status: 'Processing', customer: 'Jane Smith', date: '2023-05-15', total: 200.00 },
+      { id: '3', status: 'Shipped', customer: 'Mike Johnson', date: '2023-05-20', total: 150.00 },
+      { id: '4', status: 'Cancelled', customer: 'Emily Davis', date: '2023-05-22', total: 120.00 }
+    ];
+  }
+  
 }
