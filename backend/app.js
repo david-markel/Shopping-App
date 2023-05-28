fa57bf1f234d3cb28a31b5dbfae5fec09d750f36
ocasodi <david.l.markel@gmail.com>
2023-05-27 20:25:10 -0400

    apis babyy

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
+const express = require("express");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
 
 const app = express();
+app.use(cors());
 const port = 3000;
-const dbUrl = 'mongodb://localhost:27017';
+const dbUrl =
+  "mongodb+srv://davidlmarkel:gd5HgmtbcNA6gxRB@cluster0.d439ezl.mongodb.net/";
 
 const client = new MongoClient(dbUrl, { useUnifiedTopology: true });
 
-client.connect()
+client
+  .connect()
   .then(() => {
     console.log("Connected successfully to mongodb");
 
-    // Assuming "markelo" is the name of your database
+    // Assuming "myNewDatabaseName" is the name of your database
     const db = client.db("markelo");
 
-    app.get('/getAllItems', async (req, res) => {
+    app.get("/getAllItems", async (req, res) => {
       try {
         let items = [];
-        const collections = ['furniture', 'clothing', 'household']; // add all your seven collections here
+        const collections = [
+          "furniture",
+          "clothing",
+          "household",
+          "movies",
+          "games",
+          "toys",
+          "groceries",
+        ];
 
-        for(const collection of collections) {
+        for (const collection of collections) {
           const collItems = await db.collection(collection).find({}).toArray();
           items = items.concat(collItems);
         }
@@ -31,7 +43,7 @@ client.connect()
       }
     });
 
-    app.get('/getItemsCategory/:category', async (req, res) => {
+    app.get("/getItemsCategory/:category", async (req, res) => {
       try {
         const category = req.params.category;
         const items = await db.collection(category).find({}).toArray();
@@ -47,6 +59,6 @@ client.connect()
       console.log(`Server listening at http://localhost:${port}`);
     });
   })
-  .catch(err => {
-    console.error('Error:', err);
+  .catch((err) => {
+    console.error("Error:", err);
   });
